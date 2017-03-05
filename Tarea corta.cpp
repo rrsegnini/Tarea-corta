6c402a12c7b1290773a7f606607e61931bc642aa
rrsegnini <roberto.t.x@hotmail.com>
2017-03-05 16:11:55 -0600

    Eimy, ahí me dice qué opina. Me parece que es así
    Agregué las clases de ListaSimple y la de Cola (NodoLista).

--- Tarea corta.cpp
+++ Tarea corta.cpp
@@ -1,14 +1,313 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+//#include <stream.h>
+
 using namespace std;
 
-int LeerArchivo()
+
+
+class nodo {
+   public:
+    nodo(int v)
+    {
+       valor = v;
+       siguiente = NULL;
+    }
+
+nodo(int v, nodo * signodo)
+    {
+       valor = v;
+       siguiente = signodo;
+    }
+
+   private:
+    int valor;
+    nodo *siguiente;
+    
+        
+   friend class lista;
+};
+
+typedef nodo *pnodo;
+
+class lista {
+   public:
+    lista() { primero = actual = NULL; }
+    ~lista();
+    
+    void InsertarInicio(int v);
+    void InsertarFinal(int v);
+    void InsertarPos (int v, int pos);
+    void EliminarInicio();
+    void EliminarFinal();
+    void EliminarPos(int pos);
+    bool ListaVacia() { return primero == NULL; } 
+    void Imprimir();
+    void Borrar(int v);
+    void Mostrar();
+    void Siguiente();
+    void Primero();
+    void Ultimo();
+    void BorrarFinal();
+    void BorrarInicio();
+    void borrarPosicion(int pos);
+    int largoLista();
+    
+   private:
+    pnodo primero;
+    pnodo actual;
+};
+
+lista::~lista()
+{
+   pnodo aux;
+   
+   while(primero) {
+      aux = primero;
+      primero = primero->siguiente;
+      delete aux;
+   }
+   actual = NULL;
+}
+
+int lista::largoLista(){
+    int cont=0;
+
+    pnodo aux;
+    aux = primero;
+    if(ListaVacia()){
+        return cont;
+    }else{
+        while(aux!=NULL){
+        aux=aux->siguiente;
+        cont++;
+    }
+    return cont;
+    }
+    
+}
+
+void lista::InsertarInicio(int v)
+{
+   if (ListaVacia())
+     primero = new nodo(v);
+   else
+     primero=new nodo (v,primero);
+}
+ 
+void lista::InsertarFinal(int v)
+{
+   if (ListaVacia())
+     primero = new nodo(v);
+   else
+     { pnodo aux = primero;
+        while ( aux->siguiente != NULL)
+          aux= aux->siguiente;
+        aux->siguiente=new nodo(v);
+      }    
+}
+
+
+void lista::InsertarPos(int v,int pos)
+{
+   if (ListaVacia())
+     primero = new nodo(v);
+   else{
+        if(pos <=1){
+          pnodo nuevo = new nodo(v);
+          nuevo->siguiente= primero;
+          primero= nuevo;     
+        }      
+        else{
+             pnodo aux= primero;
+             int i =2;
+             while((i != pos )&&(aux->siguiente!= NULL)){
+                   i++;
+                   aux=aux->siguiente;
+             }
+             pnodo nuevo= new nodo(v);
+             nuevo->siguiente=aux->siguiente;
+             aux->siguiente=nuevo;
+             
+        }
+        }
+}
+      
+void lista::BorrarFinal()
+{
+    if (ListaVacia()){
+     cout << "No hay elementos en la lista:" << endl;
+    
+   }else{
+        if (primero->siguiente == NULL) {
+                primero= NULL;
+            } else {
+
+                pnodo aux = primero;
+                while (aux->siguiente->siguiente != NULL) {
+                    aux = aux->siguiente;
+
+                }
+                
+              pnodo temp = aux->siguiente;
+              aux->siguiente= NULL;
+
+                
+                delete temp;
+            }
+        }
+}
+
+void lista::BorrarInicio()
+{
+    if (ListaVacia()){
+     cout << "No hay elementos en la lista:" << endl;
+    
+   }else{
+        if (primero->siguiente == NULL) {
+                primero= NULL;
+            } else {
+
+                pnodo aux = primero;
+                primero=primero->siguiente;                
+                delete aux;
+            }
+        }
+}
+
+
+
+void lista:: borrarPosicion(int pos){
+     if(ListaVacia()){
+              cout << "Lista vacia" <<endl;
+    }else{
+         if((pos>largoLista())||(pos<0)){
+        cout << "Error en posicion" << endl;
+        }else{
+        if(pos==1){
+        primero=primero->siguiente;
+        }else{
+          int cont=2;
+            pnodo aux=  primero;
+            while(cont<pos){
+             aux=aux->siguiente;
+             cont++;
+            }
+            aux->siguiente=aux->siguiente->siguiente;
+            }
+        }
+     }
+
+}
+ 
+
+void lista::Mostrar()
+{
+   nodo *aux;
+   
+   aux = primero;
+   while(aux) {
+      cout << aux->valor << "-> ";
+      aux = aux->siguiente;
+   }
+   cout << endl;
+}
+
+void lista::Siguiente()
+{
+   if(actual) actual = actual->siguiente;
+}
+
+void lista::Primero()
+{
+   actual = primero;
+}
+
+void lista::Ultimo()
+{
+   actual = primero;  
+   if(!ListaVacia()) 
+      while(actual->siguiente) Siguiente();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class NodoLista 
+{
+   private:
+   int frente;
+   int fondo;
+   pnodo Cola [5]={0};
+   int tamano;
+
+public:
+       NodoLista(){
+       frente = 0;
+       fondo = -1;
+       tamano = sizeof(Cola)/sizeof(int);
+        for(int i =0;i<tamano;i++){
+         Cola[i]=0;    
+           }
+       }
+       
+      bool ColaVacia(){return fondo < frente;}        
+	  void insertar(pnodo v); 
+	  void eliminar ();
+	  void imprimir();
+	  int LeerArchivo();
+};
+
+void NodoLista:: insertar (pnodo v)
+{
+         if(fondo < tamano){
+             fondo++;
+             Cola[fondo]= v; 
+           
+         }
+         else{
+         cout<<"La cola esta llena";  
+         }
+}   
+
+void NodoLista:: eliminar ()
+{
+         if(!ColaVacia()){
+            frente++;
+         }
+         else{
+         cout<<"La cola esta vacia";  
+         }
+}  
+
+void NodoLista:: imprimir(){
+     for(int i = frente;i<fondo+1;i++){
+         cout<<Cola[i]<<"->";    
+           }
+}
+
+
+
+
+int NodoLista::LeerArchivo()
 	{
-	
-	                                   
-	
-	//Este es el que lee línea por línea[
+	//Este es el que lee lÃ­nea por lÃ­nea[
 	
 	string line;
   	ifstream myfile ("Arch1.txt");
@@ -16,41 +315,25 @@ int LeerArchivo()
   		{
     	while ( getline (myfile,line) )
     		{
-      		cout << line << '\n'; //En line está la línea del .txt
+      		cout << line << '\n'; //En line estÃ¡ la lÃ­nea del .txt
     		}
     	myfile.close();
   		}
   	else cout << "Unable to open file"; 
   	return 0;
-  	
-	
-	/*
-  	//Este es el que lo lee todo pegado[
-  	char str[256];
-
-	//std::cout << "Arch1.txt";
-	//std::cin.get (str,256);    // get c-string
-	
-	std::ifstream is("Arch1.txt");     // open file
-	
-	char c;
-	  
-	while (is.get(c))          // loop getting single characters
-		//if (c == 'o')
-			std::cout << c << "\n";
-	
-	is.close();                // close file
-	
-	return 0;
-  	//]
-  	*/
-  	
 	}
 
 int main()
 	{
-	LeerArchivo();
+	NodoLista ExpOriginal;
+	// = new NodoLista();
+	ExpOriginal.LeerArchivo();
+	
+	
+	pnodo Archivo1;
+	
+	ExpOriginal.insertar(Archivo1);
+	ExpOriginal.imprimir();
 	}
 
 // prueba
-
